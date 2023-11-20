ea33dbcf56e20407a760888a4419b620253ca362
LBOLISET <lboliset@iu.edu>
2023-11-20 01:22:27 -0500

    matplotlib

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 
 # Load data
 df = pd.read_csv("predictive_maintenance.csv")
@@ -21,9 +21,16 @@ selected_series = df[selected_feature]
 # Convert the series to a NumPy array
 selected_array = selected_series.to_numpy()
 
-# Create a histogram with KDE plot using Seaborn
+# Create a histogram with KDE plot using Matplotlib
 fig, ax = plt.subplots()
-sns.histplot(data=selected_array, kde=True, ax=ax)
+ax.hist(selected_array, bins='auto', density=True, alpha=0.7, color='blue', label='Histogram')
+kde = st.gaussian_kde(selected_array)
+x_vals = np.linspace(selected_array.min(), selected_array.max(), 100)
+ax.plot(x_vals, kde(x_vals), color='red', label='KDE')
+ax.set_xlabel(selected_feature)
+ax.legend()
+
+# Display the plot
 st.pyplot(fig)
 
 # Display data table
